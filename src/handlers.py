5e9aca67ec65d26c59be9dc06b30ac05b7827201
Wizer27 <svd2yar@gmail.com>
2026-01-15 20:48:28 +0300

    bot coding

--- src/handlers.py
+++ src/handlers.py
@@ -2,6 +2,9 @@ from aiogram import Bot,Dispatcher,F,Router
 from aiogram.filters import CommandStart,Command
 from aiogram.types import Message,File,Video,PhotoSize
 import aiogram
+from database.core import is_user_exists,create_user,subscribe,is_user_subbed
+from database.files_database.files_core import create_file,delete_file,get_user_files
+from keyboards import main_keyborad
 
 router = Router()
 
---
+++ src/keyboards.py
@@ -0,0 +1,6 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
+
+
+main_keyborad = ReplyKeyboardMarkup(keyboard =  [
+    [KeyboardButton(text = "Profile"),KeyboardButton(text = "My files")]
+])
